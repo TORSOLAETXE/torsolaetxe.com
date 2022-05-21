ab495bea0dfdb3635760ddf46c5a29e19ce1fe75
TORSOLAETXE <solaecheaitor@gmail.com>
2022-05-21 19:56:28 +0100

    Delete old

--- src/components/CloseSide.jsx
+++
@@ -1,9 +0,0 @@
-// Close sidebar
-
-const closeSide = () => {
-    document.getElementById('side--menu').style.width = '0';
-    document.getElementById('header').style.marginLeft = '0';
-    document.getElementById('main').style.marginLeft = '0';
-}
-
-export default closeSide
--- src/components/OpenSide.jsx
+++
@@ -1,9 +0,0 @@
-// Open sidebar
-
-const openSide = () => {
-    document.getElementById('side--menu').style.width = "70%";
-    document.getElementById('header').style.marginLeft = "70%";
-    document.getElementById('main').style.marginLeft = "70%";
-};
-
-export default openSide;
